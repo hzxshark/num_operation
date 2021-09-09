bfaa82eaa5fde719686524359b22e5daa5e82bf6
hzxshark <hzx1909107119@126.com>
2021-09-09 22:43:42 +0800

    feat: add readme

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 ## 解决js运算浮点数的问题
 
+## 安装
+
+`npm i num_operation`
+
+## 使用
+
+```js
+import { plus, minus, times, divide  } from 'num_operation';
+
+/** 加法 */
+plus(0.1,0.2)  // 0.3
+
+/** 减法 */
+minus(0.1,0.2) // -0.1
+
+/** 乘法 */
+times(0.1,0.2) // 0.02
+
+/** 除法 */
+divide(0.1,0.2) // 0.5
+
+
+```
+
+
--- index.ts
+++ index.ts
@@ -18,7 +18,6 @@ function strip(num: numType, precision = 15): number {
  * @param {*number} num Input number
  */
 function digitLength(num: numType): number {
-  // Get digit length of e
   const eSplit = num.toString().split(/[eE]/);
   const len = (eSplit[0].split('.')[1] || '').length - +(eSplit[1] || 0);
   return len > 0 ? len : 0;
